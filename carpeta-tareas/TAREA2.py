8cc14088ad7345588559b9d7545e77b5bb793c72
vargassergio1157 <vargas.sergio1157@gmail.com>
2019-08-29 20:46:47 -0500

    segunda tares

---
+++ carpeta-tareas/TAREA2.py
@@ -0,0 +1,10 @@
+cadena="esto se debe de imprimir al reves con while"
+print(dir(cadena))
+
+contador=0
+
+while contador <= len(cadena):
+    print(cadena[-contador])
+    contador=contador+1
+
+        
